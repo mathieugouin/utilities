de6a2d192879b732f7c683fc2cbf7fe5bad5655a
Mathieu Gouin <mgouin@gmail.com>
2023-08-18 15:31:23 -0400

    Update 0000_auto_image_sort.sh

--- 0000_auto_image_sort.sh
+++ 0000_auto_image_sort.sh
@@ -4,7 +4,9 @@
 #identify -format '%[EXIF:Model]\n' IMG_2202.JPG
 
 # Date
-#identify -format %[EXIF:DateTime] IMG_2202.JPG
+#identify -format '%[EXIF:DateTime]\n' IMG_2202.JPG
+
+# Auto folder sort
 
 # TBD Adjust here
 rel_path=..
@@ -13,10 +15,14 @@ ls *.[jJ][pP][gG] | while read f
 do
     echo Procesing image: $f ...
     d=$(identify -format '%[EXIF:DateTime]\n' "$f" | sed 's/:/-/g;s/ .*//')
+    if [ -z "$d" ]
+    then
+        echo Image $f does not contain any date information, skipping.
+        continue
+    fi
     y=$(echo $d | cut -d '-' -f 1)
     final_dir=$rel_path/$y/$d
     mkdir -p $final_dir
     echo "    moving to $final_dir"
     mv "$f" $final_dir/
 done
-
