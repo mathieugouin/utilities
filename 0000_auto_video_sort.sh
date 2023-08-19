adfee8370389dc509e9860809974474c3eb513b9
Mathieu Gouin <mgouin@gmail.com>
2023-08-19 19:23:50 -0400

    Add files via upload

---
+++ 0000_auto_video_sort.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Date
+#ffmpeg -i $f -dump 2>&1 | grep creation_time | head -n 1
+
+# Auto folder sort
+
+# TBD Adjust here
+rel_path=../001-Vidéos_triés
+find -iname "*.mp4" | while read f
+do
+    echo Procesing video: $f ...
+    d=$(ffmpeg -i $f -dump 2>&1  | grep creation_time | head -n 1 | grep -Po '\d\d\d\d-\d\d-\d\d')
+    if [ -z "$d" ]
+    then
+        echo Video $f does not contain any date information, skipping.
+        continue
+    fi
+    y=$(echo $d | cut -d '-' -f 1)
+    final_dir="$rel_path/$y/$d"
+    mkdir -p $final_dir
+    echo "    moving to $final_dir"
+    mv -i "$f" $final_dir/
+done
